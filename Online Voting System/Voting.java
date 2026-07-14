d973bc161ef33e514c55ffbebcbf752195bd0ffc
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-15 00:29:38 +0530

    Implement voting eligibility check in Voting class

---
+++ Online Voting System/Voting.java
@@ -0,0 +1,11 @@
+public class Voting {
+
+    void vote(int age) {
+
+        if (age >= 18) {
+            System.out.println("Eligible for Voting");
+        } else {
+            System.out.println("Not Eligible for Voting");
+        }
+    }
+}
