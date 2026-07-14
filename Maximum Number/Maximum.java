60cdc06c9195b504c9f19d4d8f6ab80d950026d5
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-15 00:33:58 +0530

    Implement findMax method to determine maximum of three numbers

---
+++ Maximum Number/Maximum.java
@@ -0,0 +1,12 @@
+public class Maximum {
+
+    int findMax(int a, int b, int c) {
+
+        if (a >= b && a >= c)
+            return a;
+        else if (b >= a && b >= c)
+            return b;
+        else
+            return c;
+    }
+}
