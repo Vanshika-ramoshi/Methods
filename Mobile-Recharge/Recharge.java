f4873f11cefe2d5796050eb59145002f5f7b8e5f
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-15 00:05:17 +0530

    Add Recharge class with recharge method

---
+++ Mobile-Recharge/Recharge.java
@@ -0,0 +1,8 @@
+public class Recharge {
+
+    void recharge(double amount) {
+
+        System.out.println("Recharge Successful");
+        System.out.println("Recharge Amount = " + amount);
+    }
+}
